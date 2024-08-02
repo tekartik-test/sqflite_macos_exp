a4b1f4ca982032b25a507f367b4cc4c80216de47
alex <alex@tekartik.com>
2024-08-02 17:44:49 +0200

    tmp change

--- sqflite_macos_exp/tool/run_ci_macos.dart
+++ sqflite_macos_exp/tool/run_ci_macos.dart
@@ -1,11 +1,13 @@
 import 'package:tekartik_build_menu_flutter/app_build_menu.dart';
 import 'package:process_run/shell_run.dart';
 import 'example_build_menu.dart';
+import 'package:path/path.dart';
 
 Future<void> main() async {
   var appPath = appTest1Path;
   await deleteDir(appPath);
-  var shell = Shell();
+  var shell = Shell().cd(dirname(appPath));
 
-  await shell.run('flutter create --template app');
+  await shell.run(
+      'flutter create --template app --platforms macos ${shellArgument(basename(appPath))}.');
 }
